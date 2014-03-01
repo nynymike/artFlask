b261dd7f9885643b02808d4e557c9fa91f5282bc
nynymike <mike@gluu.org>
2014-03-01 09:19:07 -0600

    minor

--- tests/data/images/convert.py
+++ tests/data/images/convert.py
@@ -3,8 +3,8 @@ import os
 
 files = os.listdir(".")
 for file in files:
-    print file
     baseFN, ext = file.split(".")
     if ext.lower()=="jpg":
         image=Image.open(file)
         image.save("%s.png" % baseFN, "PNG")
+        print "converted %s to %s.png" % (file, baseFN)
