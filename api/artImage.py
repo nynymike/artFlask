b721e52a4f00daf7a65877f6b6b03d71fa4de70a
nynymike <mike@gluu.org>
2014-03-01 10:27:09 -0600

    Updated MIME type of picture URL

--- api/artImage.py
+++ api/artImage.py
@@ -7,16 +7,21 @@ import json
 import io
 
 def get_image(art_id,action_type):
-	app_ctx =ApplicationContext('art')
-	item = app_ctx.get_item(art_id)
-	if action_type=='view':
-		return render_template('artView.html',art=item)
-	imagedir = current_app.config['UPLOAD_FOLDER']
-	extension = 'png'
-	if action_type=="thumbnail":
-		fn = "%s/%s_tn.%s"%(imagedir,art_id,extension)
-	if action_type=="picture":
-		fn = "%s/%s.%s"%(current_app.config['UPLOAD_FOLDER'],art_id,extension)
-	if action_type=="qrcode":
-		fn = '%s/qrcode.png' % (imagedir)
-	return  send_file(fn, mimetype='image/png')
+    app_ctx =ApplicationContext('art')
+    item = app_ctx.get_item(art_id)
+    if action_type=='view':
+        return render_template('artView.html',art=item)
+    imagedir = current_app.config['UPLOAD_FOLDER']
+    extension = 'png'
+    if action_type=="thumbnail":
+        fn = "%s/%s_tn.%s"%(imagedir,art_id,extension)
+    if action_type=="picture":
+        fn = "%s/%s.%s"%(current_app.config['UPLOAD_FOLDER'],art_id,extension)
+    if action_type=="qrcode":
+        fn = '%s/%s_qr.png' % (imagedir, art_id)
+
+    # Set the right return MIME type
+    returnMime = 'image/png'
+    ext = fn.split(".")[1].lower()
+    if ext == 'jpg': returnMime = 'image/jpeg'
+    return send_file(fn, mimetype=returnMime)
